5bba083fc4251a3f454aaca9de336d2c4787110e
FlorisDeVries <florisdev@hotmail.com>
2017-11-01 16:30:00 +0100

    Start wheel movement

---
+++ MarsWalker3D/Assets/Scripts/WheelRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelRotation : MonoBehaviour {
+
+	public Rigidbody rb;
+
+	public void Update()
+	{
+		transform.RotateAround(transform.position, rb.transform.right, 1f);
+	}
+}
