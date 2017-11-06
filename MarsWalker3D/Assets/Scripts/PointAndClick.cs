b3c280154f157057e036d87642aa15f2780d4518
FlorisDeVries <florisdev@hotmail.com>
2017-11-06 10:48:30 +0100

    Start on Point and Click

---
+++ MarsWalker3D/Assets/Scripts/PointAndClick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointAndClick : MonoBehaviour {
+
+	public float range;
+	public Camera camera;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+	if (Input.GetMouseButtonDown(0)){ 
+		Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit, range)){
+		// the object identified by hit.transform was clicked
+		// do whatever you want
+     }
+   }
+	}
+}
