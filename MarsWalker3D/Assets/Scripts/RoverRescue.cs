e8e3bd221face77dd078278b880dddcc489919a7
FlorisDeVries <florisdev@hotmail.com>
2017-11-07 15:46:08 +0100

    Minor changes

---
+++ MarsWalker3D/Assets/Scripts/RoverRescue.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoverRescue : MonoBehaviour {
+
+	public AudioSource source;
+
+	void OnDestroy()
+	{
+		source.Stop();
+	}
+}
