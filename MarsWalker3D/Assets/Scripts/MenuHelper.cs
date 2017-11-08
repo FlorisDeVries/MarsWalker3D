10e56304b4e55b237860648ef585808abf258f4d
FlorisDeVries <florisdev@hotmail.com>
2017-11-08 15:44:45 +0100

    More menu work

--- MarsWalker3D/Assets/Scripts/MenuHelper.cs
+++ MarsWalker3D/Assets/Scripts/MenuHelper.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MenuHelper : MonoBehaviour {
+
+	public Transform menu;
+	public Transform howToPlay;
 	public void StartGame(){
 		Application.LoadLevel("MainGame");
 	}
 
+	public void MainMenu(){
+		Application.LoadLevel("MainMenu");
+	}
+
 	public void QuitGame(){
 		Application.Quit();
+	}
+	public void HowToPlay(){
+		menu.gameObject.SetActive(false);
+		howToPlay.gameObject.SetActive(true);
+	}
+
+	public void QuitHowToPlay(){
+		menu.gameObject.SetActive(true);
+		howToPlay.gameObject.SetActive(false);
 	}
 }
