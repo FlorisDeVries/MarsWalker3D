cec3f51554125a2c7704dd73f2bf04ec63b8f611
FlorisDeVries <florisdev@hotmail.com>
2017-11-08 12:38:32 +0100

    Sound balance

--- MarsWalker3D/Assets/Scripts/CarSoundController.cs
+++ MarsWalker3D/Assets/Scripts/CarSoundController.cs
@@ -7,15 +7,28 @@ public class CarSoundController : MonoBehaviour {
 	public Rigidbody rb;
 	public AudioSource[] sources;
 
-	float pitch, volume;
+	public float maxPitch, maxVolume;
+
+	float pitch, volume, tempPitch;
 	
+	private void Start() {
+		tempPitch = maxPitch;
+	}
 	// Update is called once per frame
 	void Update () {
 		float velocity = Mathf.Min(Mathf.Abs(rb.velocity.x) + Mathf.Abs(rb.velocity.z), 10);
 		if(velocity <= 0 && !Input.GetKeyDown(KeyCode.W) && !Input.GetKeyDown(KeyCode.S))
 			volume = 0;
-		pitch = Mathf.Min(velocity / 6, 1.5f);
-		volume = velocity / 6;
+
+		if(velocity/6 < maxPitch){
+			pitch = velocity / 6;
+			tempPitch = maxPitch;
+		} else {
+			tempPitch += Random.Range(0, 2) == 1 ? .01f : -.01f;
+			tempPitch = Mathf.Max(maxPitch, Mathf.Min(tempPitch, velocity/6));
+			pitch = tempPitch;
+		}
+		volume = Mathf.Min(velocity / 6, maxVolume);
 		foreach(AudioSource source in sources){
 			source.volume = volume;
 			source.pitch = pitch;
