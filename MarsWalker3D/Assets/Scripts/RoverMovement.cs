f365eeac19185c59ef389e0d5e57931fa68d1ed5
FlorisDeVries <florisdev@hotmail.com>
2017-11-08 13:32:44 +0100

    Bug fixes, car movement, radio highlight

--- MarsWalker3D/Assets/Scripts/RoverMovement.cs
+++ MarsWalker3D/Assets/Scripts/RoverMovement.cs
@@ -7,27 +7,47 @@ public class RoverMovement : MonoBehaviour {
 	public List<AxleInfo> axleInfos;
 	public float maxMotorTorque;
 	public float maxSteeringAngle;
-	public float breakTorque;
-
-	private Vector3 centerOfMassOffset =  new Vector3(0,-.5f,0);
+	public float brakeTorque;
+	public float maxSpeed;
 	public Rigidbody rb;
+	public bool finished = false;
+	private Vector3 centerOfMassOffset =  new Vector3(0,-.5f,0);
 
 	void Start() {
 		rb.centerOfMass += centerOfMassOffset;
 	}
-	void FixedUpdate() {
-		float motor = maxMotorTorque * Input.GetAxis("Move");
+	void Update() {
+		if(finished){
+			foreach (AxleInfo axleInfo in axleInfos)
+			{
+				axleInfo.leftWheel.brakeTorque = brakeTorque;
+				axleInfo.rightWheel.brakeTorque = brakeTorque;
+			}
+			return;
+		}
+
+		float speed = rb.velocity.sqrMagnitude;
+		float motor = speed < maxSpeed ? maxMotorTorque * Input.GetAxis("Move") : 0;
 		float steering = maxSteeringAngle * Input.GetAxis("Turn");
 
 		foreach (AxleInfo axleInfo in axleInfos){
-				if (axleInfo.steering) {
-                    axleInfo.leftWheel.steerAngle = steering;
-                    axleInfo.rightWheel.steerAngle = steering;
-                }
-                if (axleInfo.motor) {
-                    axleInfo.leftWheel.motorTorque = motor;
-                    axleInfo.rightWheel.motorTorque = motor;
-                }
+				if(Input.GetAxis("Brake") == 0){
+					if (axleInfo.steering) {
+						axleInfo.rightWheel.brakeTorque = 0;
+						axleInfo.leftWheel.brakeTorque = 0;
+						axleInfo.leftWheel.steerAngle = steering;
+						axleInfo.rightWheel.steerAngle = steering;
+					}
+					if (axleInfo.motor) {
+						axleInfo.leftWheel.motorTorque = motor;
+						axleInfo.rightWheel.motorTorque = motor;
+					}
+				} else{
+					if(axleInfo.steering){
+						axleInfo.rightWheel.brakeTorque = brakeTorque;
+						axleInfo.leftWheel.brakeTorque = brakeTorque;
+					}
+				}
 		}
 	}
 }
