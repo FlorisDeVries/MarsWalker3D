7d949c50c0617dd280dfe920ba2241b15d5b6ef7
FlorisDeVries <florisdev@hotmail.com>
2017-11-09 11:23:30 +0100

    cursor fix

--- MarsWalker3D/Assets/Scripts/PointAndClick/PointAndClick.cs
+++ MarsWalker3D/Assets/Scripts/PointAndClick/PointAndClick.cs
@@ -7,13 +7,14 @@ public class PointAndClick : MonoBehaviour {
 	public float range;
 	public Camera cam;
 	public ResearchProgress researchProgress;
+	public Transform rover;
 	
 	void Update () {
 		if (Input.GetMouseButtonDown(0)){ 
 			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
 			RaycastHit hit;
-			if (Physics.Raycast(ray, out hit, range))
-				if(hit.collider.tag == "Clickable"){
+			if (Physics.Raycast(ray, out hit))
+				if(hit.collider.tag == "Clickable" && Vector3.Distance(rover.position, hit.collider.transform.position) < range){
 					var collider = hit.collider;
 					var clickable = collider.GetComponent<ClickAble>();
 					clickable.OnClick();
